2debe1eb416aa97294cd9aaf03e0697677ddf1bb
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2014-06-23 22:32:50 +0200

    Simple unit test

---
+++ ticonfeu/app/lib/calculator.js
@@ -0,0 +1,5 @@
+var sum =  function(a,b){
+	return a + b;
+};
+
+exports.sum = sum;
--- ticonfeu/spec/testframework_spec.js
+++ ticonfeu/spec/testframework_spec.js
@@ -2,4 +2,14 @@ describe("An awesome test framework", function(){
 	it("should work without pain", function(){
 		expect(true).toBe(true); 
 	});
+
+	it("should let you run a simple unit test", function(){
+		expect(1+1).toBe(2);
+	});
+
+	it("should let you unit test common js modules", function(){
+		var c = require("calculator"); //You always need one
+		expect(c.sum(1,3)).toBe(4);
+	});
+	
 });
